e790483cdcf1bb46e5681351a70a6b33883250ad
Cassis <cassismjones@gmail.com>
2020-05-13 14:04:44 +0100

    Testing with Capybara

--- app.rb
+++ app.rb
@@ -21,13 +21,18 @@ get '/latest' do
   "This should be the newest info"
 end
 
-get '/random-cat' do
-    @names = ["Amigo", "Chan", "Viking"].sample
-    erb (:index)
+get '/cat-form' do
+  erb :cat_form
 end
 
-get '/named-cat' do
+post '/named-cat' do
+  p params
   @names = params[:name]
-  erb (:index)
+  erb :index
 end
 
+
+# get '/random-cat' do
+#     @names = ["Amigo", "Chan", "Viking"].sample
+#     erb (:index)
+# end
