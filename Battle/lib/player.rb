ce754a3458036dbc2efa34b6cd0a8cf2ca159ca2
Student <student@Makerss-Air-2.broadband>
2020-05-14 14:30:25 +0100

    Have started the player class and wrote tests for functionality

---
+++ Battle/lib/player.rb
@@ -0,0 +1,16 @@
+class Player 
+  def initialize(name, hp)
+    @name = name 
+    @hp = hp
+  end 
+
+  #attck method
+  def attack 
+    #should call reduce hp
+  end
+
+  #reduce hp method
+  def reduce_hp
+    # @hp -=10
+  end
+end
---
+++ Battle/spec/player_spec.rb
@@ -0,0 +1,29 @@
+require "./lib/player.rb"
+
+describe Player do
+  cj = Player.new("CJ", 60)
+
+  it "should create a new instance of itself" do 
+    expect(cj).to be_instance_of Player
+  end
+
+  describe "#attack" do 
+    it "should respond to an attack method" do
+      expect(cj).to respond_to :attack
+    end
+
+    it "should call the reduce_hp method and return hp" do
+      expect(cj.attack).to eq 50
+    end
+  end
+
+  describe "#reduce_hp" do
+    it "should respond to reduce_hp method" do
+      expect(cj).to respond_to :reduce_hp
+    end
+
+    it "should take the hp value and return 10 less" do
+      expect(cj.reduce_hp).to eq 50
+    end
+  end
+end
