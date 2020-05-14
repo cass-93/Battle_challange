1a486d5ce9c0f9f0475599c818ae108f01789121
Cassis <cassismjones@gmail.com>
2020-05-14 16:03:49 +0100

    Skinny controllers refactored

--- Battle/lib/game.rb
+++ Battle/lib/game.rb
@@ -1,5 +1,15 @@
-class Game 
-  def initialize
+class Game
+  def initialize(player_1, player_2)
+    @player_1 = player_1
+    @player_2 = player_2
+  end
+
+  def player_1
+    @player_1
+  end
+
+  def player_2
+    @player_2
   end
 
   def attack(player)
--- Battle/spec/game_spec.rb
+++ Battle/spec/game_spec.rb
@@ -1,8 +1,10 @@
 require "./lib/game.rb"
 
 
-describe Game do 
+describe Game do
+  subject(:game) { described_class.new(cj, jerome) }
   let(:cj) { double :Player, reduce_hp: 50}
+  let(:jerome) { double :Player, reduce_hp: 50}
 
   it "should create a new instance of itself" do
     expect(subject).to be_instance_of Game
@@ -15,6 +17,12 @@ describe Game do
 
     it "should call the reduce_hp method and return hp" do
       expect(subject.attack(cj)).to eq 50
+    end
+  end
+
+  describe "#player_1" do
+    it "gives us the first player" do
+      expect(subject.player_1).to eq cj
     end
   end
 end
